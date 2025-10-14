f13f9ef2c0c4eee488f060740c90d47524f72e62
dementoh <anhchangdemen019@gmail.com>
2025-10-14 19:14:13 -0400

    Change tsconfig and add build config

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,24 @@
 import express from "express";
-import taskRoutes from "./routes/tasksRoutes.ts";
-import userRoutes from "./routes/userRoutes.ts";
+import taskRoutes from "./routes/tasksRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import dotenv from "dotenv";
-import connectDB from "./config/db.ts";
+import connectDB from "./config/db.js";
 import cors from "cors";
 import session from "express-session";
-import passport from "./config/passport.ts";
+import passport from "./config/passport.js";
+import path, { dirname, join } from "path";
+import { fileURLToPath } from "url";
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const PORT = process.env.PORT || 5001;
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+}
 
 app.use(
   session({
@@ -24,6 +31,14 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use("/api/tasks", taskRoutes);
 app.use("/auth", userRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(join(__dirname, "../frontend/dist")));
+  app.get(/.*/, (_req, res) => {
+    res.sendFile(join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
